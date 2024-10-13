79138984c074f00c20ff2b05ef2ddfcdfe9d946b
Academe33 <163585104+Academe33@users.noreply.github.com>
2024-10-12 23:58:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function voltarPagina() {
-    window.history.back();
+body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    background-color: #f0f0f0;
+}
+
+.image-container {
+    overflow: hidden;
+    position: relative;
+    width: 80vw; /* Largura do contêiner */
+    height: 80vh; /* Altura do contêiner */
+}
+
+#image {
+    width: 100%;
+    height: auto;
+    transition: transform 0.2s; /* Animação suave para o zoom */
+    cursor: pointer; /* Muda o cursor para indicar que a imagem é interativa */
 }
